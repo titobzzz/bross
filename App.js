16bb5263e6cad655dad0539714f54f47ca5a1cd8
titobzzz <105544565+titobzzz@users.noreply.github.com>
2022-10-23 14:24:56 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import Navbar from "./components/nav";
+import Sec from "./components/sec";
+
+
+export default function App() {
+    return (
+        <div>
+            <Navbar/>
+            <Sec/>
+            
+        </div>
+    )
+}
+
+
+       
+
+
